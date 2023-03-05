216cbdb84224172b3f32780f86e5c18d3cfa5622
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-03-05 18:12:58 -0500

    Update Token.java

--- src/Token.java
+++ src/Token.java
@@ -3,15 +3,23 @@ public class Token {
     String lexeme;
     TokenCodes tokenCode;
 
-    Token(String lex, TokenCodes code){
+    Token(String lex, TokenCodes code){		//creation of a Token object
         lexeme = lex;
         tokenCode = code;
     }
 
-    void print (){
-        System.out.print("Lexeme: '"+lexeme+"'" );
-        for (int i=0 ; i<=12-lexeme.length();i++){System.out.print(" ");}
-        System.out.print(" Reserved Word: " + tokenCode);
+    
+    /* no-parameter print function will print the selected lexeme
+     *  and its corresponding token code */
+    
+    void print (){									
+        
+    	System.out.print("Lexeme: '"+lexeme+"'" );
+        
+        for (int i=0 ; i<=12-lexeme.length();i++){ 		//creates a gap between columns
+        	System.out.print(" ");
+        	}
+        System.out.print(" TokenCode: " + tokenCode);
         System.out.println();
     }
 
