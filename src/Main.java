860b3fcc55afea4cb506f73701349af4b53da8b6
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-03-05 18:13:33 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.FileNotFoundException;  // Import to handle errors
 
 
 public class Main {
     public static void main(String[] args) {
 
         try {
-            File file = new File("src/test1.pas");
+        //read the file and create a LexicalAnlyzer object with the provided file
+            File file = new File("/Users/sotirisemmanouil/eclipse-workspace/CS316-project1/src/test.txt");
             LexicalAnalyzer la = new LexicalAnalyzer(file);
             la.output();
 
-        } catch (FileNotFoundException e) {
+        } 
+        
+        //enabled exception handling if for various reasons, the file cannot be opened.
+        catch (FileNotFoundException e) {
             System.out.println("Error: Cant read the input file!");
             e.printStackTrace();
         }
+        
 
     }
 }
